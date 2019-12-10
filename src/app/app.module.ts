d29e730c714d6e0039c82e866588d148af4612db
balasuresha <balasuresh.asai@gmail.com>
2019-12-10 13:18:54 +0530

    Committed the design integrated files in git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JobsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
